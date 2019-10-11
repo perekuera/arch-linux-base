30de5eb5d91be8d844442bc597898a6eb3c803ad
Pere <perekuera@gmail.com>
2019-10-11 16:41:24 +0200

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -41,17 +41,19 @@ function create_partition_efi()
 function create_partition_bios()
 {
 	echo Create BIOS partition table...
-	{
-	test
-	sfdisk /dev/$DISK < $PARTITION_DATA_FILE
-	sfdisk -d /dev/$DISK
-	exit 0
-	sfdisk /dev/$DISK <<EOF
-start=512,size=$((BOOT_PARTITION_SIZE*2*1024)),type=83,bootable
-size=$((SWAP_PARTITION_SIZE*2*1024)),type=82
-type=83
-EOF
-	} > /dev/nul
+	echo start=512,size=$((BOOT_PARTITION_SIZE*2*1024)),type=83,bootable > $PARTITION_DATA_FILE
+	if [ $SWAP_PARTITION_SIZE -gt 0 ]; then
+		echo size=$((SWAP_PARTITION_SIZE*2*1024)),type=82 >> $PARTITION_DATA_FILE
+	fi
+	if [ $ROOT_SIZE -eq 0 ]; then
+		# all for /
+		echo type=83 >> $PARTITION_DATA_FILE
+	else
+		echo size=$((ROOT_SIZE*2*1024)),type=83 >> $PARTITION_DATA_FILE
+		# rest for /home
+		echo type=83 >> $PARTITION_DATA_FILE
+	fi
+	sfdisk /dev/$DISK < $PARTITION_DATA_FILE > /dev/nul
 	sfdisk -d /dev/$DISK
 }
 

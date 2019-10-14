933bbc16c6825954b9b56faa89b25d53d0fe6c1f
perekuera <perekuera@gmail.com>
2019-10-14 19:00:09 +0200

    testing

--- installation/src/base-installation.sh
+++ installation/src/base-installation.sh
@@ -19,15 +19,15 @@ function install_base_configurations()
 	sleep 1
 	genfstab -pU /mnt >> /mnt/etc/fstab
     echo ">>> arch-chroot!!!!"
-	arch-chroot /mnt /bin/bash <<EOF
-        echo $HOST_NAME > /etc/hostname
-        ln -sf $TIME_ZONE /etc/localtime
-        sed -i "s/#${LOCALE_CONF}/${LOCALE_CONF}/g" /etc/locale.gen
-        echo LANG=$LOCALE_CONF > /etc/locale.conf
-        locale-gen
-        hwclock -w
-        echo KEYMAP=$KEYMAP > /etc/vconsole.conf
-    EOF
+arch-chroot /mnt /bin/bash <<EOF
+echo $HOST_NAME > /etc/hostname
+ln -sf $TIME_ZONE /etc/localtime
+sed -i "s/#${LOCALE_CONF}/${LOCALE_CONF}/g" /etc/locale.gen
+echo LANG=$LOCALE_CONF > /etc/locale.conf
+locale-gen
+hwclock -w
+echo KEYMAP=$KEYMAP > /etc/vconsole.conf
+EOF
 }
 
 function user_configurations()

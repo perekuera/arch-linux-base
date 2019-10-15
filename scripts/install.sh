55f7093c95260506a462d9c85d94a30e2337b399
perekuera <perekuera@gmail.com>
2019-10-15 17:22:47 +0200

    testing

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
+function print() {
+    printf "<<<\n\t$1\n>>>"
+}
+
 source ./setup.conf
 
-echo "Installation disk $DISK"
+# Display configuration
+print "Installation disk: $INSTALLATION_DISK"
+
+sleep 1
+
+###########################
+### UEFI/BIOS detection ###
+###########################
 
 efivar -l >/dev/null 2>&1
 
 if [[ $? -eq 0 ]]; then
     UEFI=1
-    echo "UEFI detected"
+    print "UEFI detected"
 else
     UEFI=0
-    echo "BIOS detected"
+    print "BIOS detected"
 fi
+
+#######################
+### Disk partitions ###
+#######################
+
+sgdisk --zap-all $INSTALLATION_DISK
+
+if [[ $UEFI -eq 1 ]]; then
+    printf "n\n1\n\n+512M\nef00\nw\ny\n" | gdisk /dev/sda
+    yes | mkfs.vfat -F32 /dev/sda1
+else
+    printf "n\np\n1\n\n+512M\nw\n" | fdisk /dev/sda
+    yes | mkfs.ext4 /dev/sda1
+fi
+

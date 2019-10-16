36faf86926387332b3ca1e569e030dbb64c912eb
perekuera <perekuera@gmail.com>
2019-10-16 17:59:03 +0200

    testing

--- scripts/install.sh
+++ scripts/install.sh
@@ -30,15 +30,20 @@ fi
 #######################
 
 #dd if=/dev/zero of=/dev/sdX  bs=512  count=1
-sgdisk --zap-all $INSTALLATION_DISK
+#sgdisk --zap-all $INSTALLATION_DISK
 
 if [[ $UEFI -eq 1 ]]; then
-    printf "n\n1\n\n+512M\nef00\nw\ny\n" | gdisk /dev/sda
-    yes | mkfs.vfat -F32 /dev/sda1
-    printf "n\n2\n\n\n8e00\nw\ny\n"| gdisk /dev/sda
+    #printf "n\n1\n\n+512M\nef00\nw\ny\n" | gdisk /dev/sda
+    #yes | mkfs.vfat -F32 /dev/sda1
+    #printf "n\n2\n\n\n8e00\nw\ny\n"| gdisk /dev/sda
+    echo label: gpt > $TEMP_PARTITION_DATA_FILE
+    echo start=512,size=1048576,type=U > $TEMP_PARTITION_DATA_FILE
+    echo type=L > $TEMP_PARTITION_DATA_FILE
 else
-    printf "n\n p\n 1\n \n +512M\n w\n" | fdisk /dev/sda
-    yes | mkfs.ext4 /dev/sda1
-    printf "n\np\n2\n\n\nt\n2\n8e\nw\n" | fdisk /dev/sda
+    #printf "n\n p\n 1\n \n +512M\n w\n" | fdisk /dev/sda
+    #yes | mkfs.ext4 /dev/sda1
+    #printf "n\np\n2\n\n\nt\n2\n8e\nw\n" | fdisk /dev/sda
+    echo label: dos > $TEMP_PARTITION_DATA_FILE
+    echo start=512,type=L,bootable > $TEMP_PARTITION_DATA_FILE
 fi
 

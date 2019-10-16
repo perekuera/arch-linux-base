83ff2b1285f8b11e888669f54ff149df81d555d1
perekuera <perekuera@gmail.com>
2019-10-16 18:03:45 +0200

    testing

--- scripts/install.sh
+++ scripts/install.sh
@@ -37,14 +37,14 @@ if [[ $UEFI -eq 1 ]]; then
     #yes | mkfs.vfat -F32 /dev/sda1
     #printf "n\n2\n\n\n8e00\nw\ny\n"| gdisk /dev/sda
     echo label: gpt > $TEMP_PARTITION_DATA_FILE
-    echo start=512,size=1048576,type=U > $TEMP_PARTITION_DATA_FILE
-    echo type=L > $TEMP_PARTITION_DATA_FILE
+    echo start=512,size=1048576,type=U >> $TEMP_PARTITION_DATA_FILE
+    echo type=L >> $TEMP_PARTITION_DATA_FILE
 else
     #printf "n\n p\n 1\n \n +512M\n w\n" | fdisk /dev/sda
     #yes | mkfs.ext4 /dev/sda1
     #printf "n\np\n2\n\n\nt\n2\n8e\nw\n" | fdisk /dev/sda
     echo label: dos > $TEMP_PARTITION_DATA_FILE
-    echo start=512,type=L,bootable > $TEMP_PARTITION_DATA_FILE
+    echo start=512,type=L,bootable >> $TEMP_PARTITION_DATA_FILE
 fi
 
-sfdisk $DISK < $TEMP_PARTITION_DATA_FILE > /dev/nul
+sfdisk $INSTALLATION_DISK < $TEMP_PARTITION_DATA_FILE > /dev/nul

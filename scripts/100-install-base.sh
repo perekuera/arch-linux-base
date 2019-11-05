d038116b9bbc8f09f340df80c0071917f746c525
Pere <perekuera@gmail.com>
2019-11-05 17:31:10 +0100

    Final base installation

--- scripts/100-install-base.sh
+++ scripts/100-install-base.sh
@@ -8,10 +8,15 @@ function print() {
 ### Configuration ###
 #####################
 
-# Installation disk
 INSTALLATION_DISK=/dev/sda
+HOST_NAME=arch
+TIME_ZONE=/usr/share/zoneinfo/Europe/Madrid
+LOCALE_CONF=es_ES.UTF-8
+KEYMAP=es
+ROOT_PASSWORD=1234
+USER_NAME=pere
+USER_PASSWORD=1234
 
-# Temporary partition config file
 TEMP_PARTITION_DATA_FILE=/tmp/temp_partition_data_file.cfg
 
 # Display configuration
@@ -45,15 +50,15 @@ sgdisk --zap-all $INSTALLATION_DISK
 if [[ $UEFI -eq 1 ]]; then
     #printf "n\n1\n\n+512M\nef00\nw\ny\n" | gdisk /dev/sda
     #yes | mkfs.vfat -F32 /dev/sda1
-    #printf "n\n2\n\n\n8e00\nw\ny\n"| gdisk /dev/sda
-    echo label: gpt > $TEMP_PARTITION_DATA_FILE
-    echo start=2048,size=1048576,type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B >> $TEMP_PARTITION_DATA_FILE
-    echo type=0FC63DAF-8483-4772-8E79-3D69D8477DE4 >> $TEMP_PARTITION_DATA_FILE
-else
-    #printf "n\n p\n 1\n \n +512M\n w\n" | fdisk /dev/sda
-    #yes | mkfs.ext4 /dev/sda1
-    #printf "n\np\n2\n\n\nt\n2\n8e\nw\n" | fdisk /dev/sda
-    echo label: dos > $TEMP_PARTITION_DATA_FILE
+    #printf "n\nROOT_PASSWORD2\n\n\n8e00\nw\ny\n"| gdisk /dev/sda
+    echo label: ROOT_PASSWORDgpt > $TEMP_PARTITION_DATA_FILE
+    echo start=2ROOT_PASSWORD048,size=1048576,type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B >> $TEMP_PARTITION_DATA_FILE
+    echo type=0FROOT_PASSWORDC63DAF-8483-4772-8E79-3D69D8477DE4 >> $TEMP_PARTITION_DATA_FILE
+elseROOT_PASSWORD
+    #printf "n\nROOT_PASSWORD p\n 1\n \n +512M\n w\n" | fdisk /dev/sda
+    #yes | mkfs.ROOT_PASSWORDext4 /dev/sda1
+    #printf "n\nROOT_PASSWORDp\n2\n\n\nt\n2\n8e\nw\n" | fdisk /dev/sda
+    echo label: ROOT_PASSWORDdos > $TEMP_PARTITION_DATA_FILE
     echo start=2048,type=83,bootable >> $TEMP_PARTITION_DATA_FILE
 fi
 
@@ -77,5 +82,43 @@ else
     mkdir /mnt/boot
 fi
 
+########################
+### Install packages ###
+########################
 
 print "Install base packages"
+
+pacstrap /mnt base base-devel grub
+
+###########################
+### Base configurations ###
+###########################
+
+print "Base configurations"
+
+genfstab -pU /mnt >> /mnt/etc/fstab
+arch-chroot /mnt /bin/bash <<EOF
+echo $HOST_NAME > /etc/hostname
+ln -sf $TIME_ZONE /etc/localtime
+sed -i "s/#${LOCALE_CONF}/${LOCALE_CONF}/g" /etc/locale.gen
+echo LANG=$LOCALE_CONF > /etc/locale.conf
+locale-gen
+hwclock -w
+echo KEYMAP=$KEYMAP > /etc/vconsole.conf
+EOF
+
+#############
+### Users ###
+#############
+
+print "Users"
+
+arch-chroot /mnt /bin/bash <<EOF
+echo "root:${ROOT_PASSWORD}" | chpasswd
+useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash $USER_NAME
+echo "${USER_NAME}:${USER_PASSWORD}" | chpasswd
+EOF
+
+umount -R /mnt
+
+#reboot

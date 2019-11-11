11beacf692955033759f40b0d71d0e114d703666
Pere <perekuera@gmail.com>
2019-11-11 17:59:16 +0100

    remove mesa

--- scripts/10-install-xorg-video-virtualbox.sh
+++ scripts/10-install-xorg-video-virtualbox.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xterm --noconfirm --needed
-sudo pacman -S mesa mesa-demos --noconfirm --needed
 
 sudo pacman -S virtualbox-guest-utils --noconfirm --needed

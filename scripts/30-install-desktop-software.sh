997ddbded34c8ac97c9bb00b43481f41ee682ed0
Pere <perekuera@gmail.com>
2019-11-28 18:38:54 +0100

    more software added

---
+++ scripts/30-install-desktop-software.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ./install.conf
+
+if [[ "$USER" != "$USER_NAME" ]]; then
+    echo "Need login with user '$USER_NAME'"
+    exit
+fi
+
+# Google Chrome
+yay -S google-chrome
+
+# Firefox
+sudo pacman -S firefox --noconfirm --needed
+
+sudo pacman -S vi vim zsh file-roller grub-customizer --noconfirm --needed
+
+#spotify
+cd ~
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 2EBF997C15BDA244B6EBF5D84773BD5E130D1D45
+git clone https://aur.archlinux.org/spotify.git
+cd spotify
+makepkg -sri
+rm -rf ~/spotify
